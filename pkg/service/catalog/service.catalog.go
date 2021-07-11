46d7f88b27afda7109cfd56a0d3c3bdf6974d285
Sholahuddin Alisyahbana <bana@qerja.com>
2021-07-11 23:35:53 +0700

    add modul alexa and google home

--- pkg/service/catalog/service.catalog.go
+++ pkg/service/catalog/service.catalog.go
@@ -3,6 +3,7 @@ package catalog
 import (
 	"fmt"
 	"github.com/alisyahbana/bcg-test/pkg/service/catalog/data"
+	"math"
 	"strings"
 )
 
@@ -27,8 +28,9 @@ type PurchaseResponse struct {
 func (s CatalogService) Purchase(payload PurchasePayload) (*PurchaseResponse, error) {
 	var items []data.Item
 	var totalPrice float64
-
+	var countGoogleHome int
 	var purchasedMacbook bool
+	var countAlexa int
 	for _, keyword := range payload.Items {
 		// if purchased macbook
 		if strings.ToLower(keyword) == "macbook pro" {
@@ -41,20 +43,33 @@ func (s CatalogService) Purchase(payload PurchasePayload) (*PurchaseResponse, er
 		if item == nil {
 			return nil, fmt.Errorf("item '%s' not found", keyword)
 		}
-
+		if item.SKU == "120P90" {
+			countGoogleHome += 1
+		}
+		if item.SKU == "A304SD" {
+			countAlexa += 1
+		}
 		price := item.Price
-
 		// if purchased macbook then free raspberry
 		if purchasedMacbook && item.SKU == "234234" {
 			price = 0
 		}
-
 		totalPrice += price
-
 		items = append(items, *item)
 	}
 
+	if countGoogleHome%3 == 0 {
+		discount := 49.99
+		numberDiscount := countGoogleHome / 3
+		totalDiscount := (discount * float64(numberDiscount))
+		totalPrice = totalPrice - totalDiscount
+	}
+
+	if countAlexa >= 3 {
+		totalPrice = totalPrice - float64(totalPrice*10/100)
+	}
+
 	return &PurchaseResponse{
-		TotalPrice: totalPrice,
+		TotalPrice: math.Round(totalPrice*100) / 100,
 	}, nil
 }
